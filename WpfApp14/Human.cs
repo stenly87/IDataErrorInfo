115a90059060e6e3eade95c44b05e27c11445cbf
ppk <ppk_student@hotmail.com>
2022-02-07 09:22:50 +1000

    Add project files.

---
+++ WpfApp14/Human.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace WpfApp14
+{
+    public class Human : IDataErrorInfo
+    {
+        public string this[string columnName]
+        {
+            get {
+                string errorInfo = null;
+                switch (columnName)
+                {
+                    case "Age":
+                        if (Age > 120)
+                            errorInfo= "Введен слишком большой возраст";
+                        else if (Age < 0)
+                            errorInfo= "Возраст не может быть отрицательным";
+                        break;
+                    case "Name":
+                        if (Name.Length < 2)
+                            errorInfo = "Имя слишком короткое";
+                        break;
+                }
+                return errorInfo;
+            }
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Error { get; private set; }
+    }
+
+}
---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Human EditHuman { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            EditHuman = new Human { Name = "Петя", Age = 20 };
+            DataContext = this;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
